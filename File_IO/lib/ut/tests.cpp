1026c94fe06916bc0ca7aa136c1be47c9c763620
Vivian <nkkyviv@gmail.com>
2023-01-24 09:14:48 +0100

    Commits on testing func

--- File_IO/lib/ut/tests.cpp
+++ File_IO/lib/ut/tests.cpp
@@ -2,14 +2,13 @@
 #include <project.h>
 #include <fstream>
 #include <stdexcept>
-
-FileIO file;
+FileIO file_m("Vivian.txt");
 
 TEST(File_IO_tests, FileWriteTest)
 {
     try
     {
-        file.write_line("Hello, this is my write function");
+        file_m.write_line("Hello, this is my write function");
     }
     catch (const std::exception &e)
     {
