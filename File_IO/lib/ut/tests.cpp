6fbbb1f32e3da08004181f4c0938f9d1cc5f8261
Vivian <nkkyviv@gmail.com>
2023-01-24 07:59:13 +0100

    First test

--- File_IO/lib/ut/tests.cpp
+++ File_IO/lib/ut/tests.cpp
@@ -2,3 +2,10 @@
 #include <project.h>
 #include <fstream>
 #include <stdexcept>
+
+FileIO network("Vivian.txt");
+TEST(FileIOTest, TestWriteFunc)
+{
+    network.write_line("lineFunc");
+    EXPECT_EQ(network.read_line(), "lineFunc");
+}
