83ea483a656e8f1d73a785596521b36b4ad4b8af
Vivian <nkkyviv@gmail.com>
2023-01-25 12:28:15 +0100

    Function to write list

--- can_messages/Can_messages.py
+++ can_messages/Can_messages.py
@@ -5,7 +5,9 @@ with open('min_signals.json','r') as file_rd:
     file_read = json.load(file_rd)
 
     print(file_read)
-    print(type(file_rd))
+    print(type(file_read))
   
+with open("my_file.txt", "w") as output_file_fd:
+    output_file_fd.writelines(file_read)
 
   
