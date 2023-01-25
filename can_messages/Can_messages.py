df54dfeb96e4d7e559a9b56615c37ccd4994adc3
Vivian <nkkyviv@gmail.com>
2023-01-25 13:10:49 +0100

    Function to write list of strings

--- can_messages/Can_messages.py
+++ can_messages/Can_messages.py
@@ -7,7 +7,15 @@ with open('min_signals.json','r') as file_rd:
     print(file_read)
     print(type(file_read))
   
-with open("my_file.txt", "w") as output_file_fd:
-    output_file_fd.writelines(file_read)
+def write_list(filename, list_to_write):
+    if filename is None or filename == " ":
+        return
+    with open(filename, 'w') as file_wrt:
+      file_wrt.writelines(list_to_write)
+
+if __name__ == "__main__":
+    write_list("my_file.txt", '%s\n' %file_read)    
+
+    
 
   
