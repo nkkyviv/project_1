6ab9d49118edc5bda98002e001bc2a3198a9e5ac
Vivian <nkkyviv@gmail.com>
2023-01-30 22:49:20 +0100

    source file update

---
+++ can_messages/output/src/min_signals.cpp
@@ -0,0 +1,42 @@
+#include "min_signals.h"
+
+#include <sstream>
+
+CAN_min_signals ::CAN_min_signals()
+{
+
+	m_startMsgId = 0x100;
+	m_temperatureGetMsgId = m_startMsgId + 2;
+	m_temperatureSetMsgId = m_startMsgId + 2 + 1 : m_humidityGetMsgId = m_startMsgId + 4;
+	m_humiditySetMsgId = m_startMsgId + 4 + 1;
+}
+std::string CAN_min_signals ::get_temperature()
+{
+	std::stringstream sstream;
+	sstream << "{\"ID\": " << m_temperatureGetMsgId
+			<< ", \"length\":0 "
+			<< ", \"value\": \"\"; }" return sstream.str();
+}
+std::string CAN_min_signals ::set_temperature(float newValue)
+{
+	std::stringstream sstream;
+	sstream << "{\"ID\": " << m_temperatureSetMsgId
+			<< ", \"length\":0 "
+			<< ", \"value\": \"\"; }"
+			<< newValue return sstream.str();
+}
+std::string CAN_min_signals ::get_humidity()
+{
+	std::stringstream sstream;
+	sstream << "{\"ID\": " << m_humidityGetMsgId
+			<< ", \"length\":0 "
+			<< ", \"value\": \"\"; }" return sstream.str();
+}
+std::string CAN_min_signals ::set_humidity(uint8_t newValue)
+{
+	std::stringstream sstream;
+	sstream << "{\"ID\": " << m_humiditySetMsgId
+			<< ", \"length\":0 "
+			<< ", \"value\": \"\"; }"
+			<< newValue return sstream.str();
+}
