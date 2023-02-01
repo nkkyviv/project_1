e54dc8d06bd2f12cec4e2d867b249b28da84e14f
Vivian <nkkyviv@gmail.com>
2023-02-01 14:27:43 +0100

    Changes to my yaml file for the whole component

--- can_messages/Can_messages.py
+++ can_messages/Can_messages.py
@@ -1,158 +1,89 @@
 import json
-path = 'my_file.txt'
-jason = 'min_signals.json'
+import os
 
 
-# The set function variable
-value_type = "newValue"
-setType = "uint8_t\t"
-
-m_startMsgId = "m_startMsgId"
-m_temperatureGetMsgId = "m_temperatureGetMsgId"
-m_temperatureSetMsgId = "m_temperatureSetMsgId"
-m_humidityGetMsgId = "m_humidityGetMsgId"
-m_humiditySetMsgId = "m_humiditySetMsgId"
-signals_id = "0x100"
-
-
-# Opening JSON file
-def read_file(filename):
-    buffer = []
-    if filename is None or filename == " ":
-        return buffer
-    with open(filename, 'r') as file_rd:
-        buffer = json.load(file_rd)
-        return buffer
+def gen_include_guard_class_decl_ctor(json_filename):
+    top = []  # include guard, class declaration and constructor
+    bottom = ""  # ending of include guard
+    json_name_upper = json_filename.upper()
+    include_guard_name = f'HEADER_{json_name_upper}_H'
+    top.append(f'#ifndef {include_guard_name}')
+    top.append(f'\n#define {include_guard_name}')
+    top.append(f'\n#include <sstream>\n')
+    top.append('\n')
+    top.append(f'class CAN_{json_filename} {{')
+    top.append('\n\tpublic:')
+    top.append(f'\n\t\tCAN_{json_filename}();')
+    bottom = ['};', f'\n#endif //{include_guard_name}']
+    return top, bottom
 
 
-with open(jason) as file_rd:
-    jason_dict = json.load(file_rd)
-    dict_jason = jason_dict
+def function_to_generate_header(signal_name, signal_type, signal_length, signal_comment):
+    output_data = "\n"
+    output_data += '\t\t/*\n\t\tget {}\n\t\t*/' .format(signal_comment)
+    output_data += "\n\t\tstd::string get_{}(); \n" .format(signal_name)
+    output_data += '\t\t/*\n\t\tset {}\n\t\t*/' .format(signal_comment)
+    output_data += "\n\t\tstd::string set_{}({} {}); \n" .format(
+        signal_name, signal_type, 'newValue')
 
-
-def write_list(filename, list_to_write):
-    if filename is None or filename == " ":
-        return
-    with open(filename, 'w') as file_wrt:
-        file_wrt.writelines(list_to_write)
-
-
-def function_to_generate_headerFile(class_name, signal_name, signal_type, value_type, signal_comment):
-    output_data = ""
-    output_data += '\t/*\n\tget {}\n\t*/' .format(signal_comment)
-    output_data += "\n\tstd::string get_{}(); \n" .format(signal_name)
-    output_data += '\t/*\n\tset {}\n\t*/' .format(signal_comment)
-    output_data += "\n\tstd::string set_{}({} {}); \n" .format(
-        signal_name, signal_type, value_type)
     return output_data
 
 
-def create_header_file():
-    header_file = "output/include/can_messages/min_signals.h"
-    myFile = open(header_file, "w")
+def generate_get_set_functions(json_dict):
+    content = []
+    temp = []
+    for signal in json_dict["signals"]:
+        signal_name = signal["name"]
+        signal_type = signal["type"]
+        signal_length = signal["length"]
+        signal_comment = signal["comment"]
+        get_prototype = function_to_generate_header(
+            signal_name, signal_type, signal_length, signal_comment)
+        content.append(get_prototype)
 
-    ifndef_def = "#ifndef HEADER_MIN_SIGNALS_H"
-    define = "#define HEADER_MIN_SIGNALS_H"
-    endif = "#endif /*HEADER_MIN_SIGNALS_H */\n"
-    include = "#include<sstream>"
-
-    myFile.write("{}\n{}\n\n".format(ifndef_def, define))
-    myFile.write("{}\n\n" .format(include))
-
-    class_can = 'class CAN_min_signals\n{'
-    public = '\npublic:\n'
-    private = '\nprivate:\n'
-    constructor = '\tCAN_min_signals();\n'
-    destructor = '\t ~CAN_min_signals();\n'
-
-    myFile.write(class_can + public)
-    myFile.write(constructor)
-    for signal_element in jason_dict["signals"]:
-        print(signal_element["name"])
-        formatted_string = f'{signal_element["type"]} and {signal_element["length"]}'
-        print(formatted_string)
-        print(signal_element["comment"])
-        myFile.write(function_to_generate_headerFile(
-            "CAN_messages", signal_element["name"], signal_element["type"], "newValue", signal_element["comment"]))
-
-    myFile.write("\nprivate:\n\t" + setType + m_startMsgId + ";" +
-                 "\n\t" + setType + m_temperatureGetMsgId + ";" +
-                 "\n\t" + setType + m_temperatureSetMsgId + ";" +
-                 "\n\t" + setType + m_humiditySetMsgId + ";" +
-                 "\n\t" + setType + m_humidityGetMsgId + ";\n")
-
-    myFile.write(destructor)
-    myFile.write("};\n" + endif)
-    myFile.close()
+    return content
 
 
-def function_to_generate_sourcefile(class_name, signal_id, signal_name, signal_length, signal_type):
-    class_name = "CAN_min_signals"
-    output_string = ""
-    output_string += "std::string {} :: get_{}() {{\n".format((class_name),
-                                                              signal_name)
-    output_string += "\tstd::stringstream sstream; \n"
-    output_string += '\tsstream << "{{\\"ID\\": "<< m_{}GetMsgId\n'.format(
-        signal_name)
-    output_string += '\t\t<< ", \\"length\\":0 " \n'.format(signal_length)
-    output_string += '\t\t<< ", \\"value\\": \\"\\"; }" \n'
-    output_string += "\treturn sstream.str(); \n"
-    output_string += "} \n"
-    output_string += "std::string {} :: set_{}({} {}) {{\n".format(class_name,
-                                                                   signal_name, signal_length, value_type)
-    output_string += "\tstd::stringstream sstream; \n"
-    output_string += '\tsstream << "{{\\"ID\\": "<< m_{}SetMsgId\n'.format(
-        signal_name)
-    output_string += '\t\t<< ", \\"length\\":0 " \n'.format(signal_length)
-    output_string += '\t\t<< ", \\"value\\": \\"\\"; }" \n'
-    output_string += '\t\t<< {}\n'.format(value_type)
-    output_string += "\treturn sstream.str(); \n"
-    output_string += "} \n"
-    return output_string
+def generate_private_fields(json_dict):
+    content = []
+    Dtype = "uint8_t"
+    content.append(f'\n\t {Dtype} m_startMsgID;')
+    for signals in json_dict["signals"]:
+        content.append(f'\n\t {Dtype} m_{signals["name"]}GetMsgID;')
+        content.append(f'\n\t {Dtype} m_{signals["name"]}SetMsgID;')
+
+    return content
 
 
-def create_source_file():
-    source_file = "output/src/min_signals.cpp"
-    myFiles = open(source_file, "w")
+def generate_header(json_filename, json_dict):
+    output = []
+    header_top, header_bottom = gen_include_guard_class_decl_ctor(
+        json_filename)
+    output += header_top
+    output += generate_get_set_functions(json_dict)
+    output.append('\n\tprivate:')
+    output += generate_private_fields(json_dict)
+    output += header_bottom  # output = output + header_bottom
+    return output
 
-    include = '#include "min_signals.h"'
-    string = "#include <sstream>"
-    myFiles.write("{}\n\n" .format(include))
-    myFiles.write("{}\n\n" .format(string))
 
-    class_Can = 'CAN_min_signals :: CAN_min_signals(){\n'
-    myFiles.write(class_Can)
-
-    myFiles.write("\n\t" + m_startMsgId + "=" + signals_id + ";"
-
-                  "\n\t" + m_temperatureGetMsgId + "=" + m_startMsgId + " + 2;"
-                  "\n\t" + m_temperatureSetMsgId + "=" + m_startMsgId + " + 2" + " + 1:"
-                  "\n\t" + m_humidityGetMsgId + "=" + m_startMsgId + " + 4;"
-                  "\n\t" + m_humiditySetMsgId + "=" + m_startMsgId + " + 4" + " + 1;\n"
-                  "}\n")
-
-    for signal_element in jason_dict["signals"]:
-        print(signal_element["name"])
-        formatted_string = f'{signal_element["type"]} and {signal_element["length"]}'
-        print(formatted_string)
-        myFiles.write(function_to_generate_sourcefile("CAN_messages", "ID",
-                                                      signal_element["name"], signal_element["type"], signal_element["length"]))
-
-    myFiles.close()
+def write_header_file(filename, data):
+    if filename is None or filename == "":
+        return
+    with open(filename, 'w') as file_fd:
+        file_fd.writelines(data)
 
 
 if __name__ == "__main__":
-    read = read_file(jason)
-    print(read)
-    print(type(read))
-    write_list(path, '%s\n' % read)
+    input_filename = "min_signals.json"
+    header_file = "output/include/can_messages/min_signals.h"
+    json_dict = {}
+    with open(input_filename) as file_fd:
+        json_raw_content = file_fd.read()
+        json_dict = json.loads(json_raw_content)
+    print(json_dict)
+    json_filename = input_filename.replace(".json", "")
+    print(json_filename)
 
-    output = function_to_generate_headerFile(
-        "CAN_min_signals", "temperature", "float", "newvalue", "the ambient temperature")
-    print(output)
-    output = function_to_generate_headerFile(
-        "CAN_min_signals", "humidity", "uint8_t", "newvalue", "the ambient humidity percentage")
-    print(output)
-
-    create_header_file()
-    create_source_file()
+    header_function = generate_header(json_filename, json_dict)
+    write_header_file(header_file, header_function)
--- can_messages/output/include/can_messages/min_signals.h
+++ can_messages/output/include/can_messages/min_signals.h
@@ -1,35 +1,32 @@
 #ifndef HEADER_MIN_SIGNALS_H
 #define HEADER_MIN_SIGNALS_H
+#include <sstream>
 
-#include<sstream>
+class CAN_min_signals {
+	public:
+		CAN_min_signals();
+		/*
+		get the ambient temperature
+		*/
+		std::string get_temperature(); 
+		/*
+		set the ambient temperature
+		*/
+		std::string set_temperature(float newValue); 
 
-class CAN_min_signals
-{
-public:
-	CAN_min_signals();
-	/*
-	get the ambient temperature
-	*/
-	std::string get_temperature(); 
-	/*
-	set the ambient temperature
-	*/
-	std::string set_temperature(float newValue); 
-	/*
-	get the ambient humidity percentage
-	*/
-	std::string get_humidity(); 
-	/*
-	set the ambient humidity percentage
-	*/
-	std::string set_humidity(uint8_t newValue); 
+		/*
+		get the ambient humidity percentage
+		*/
+		std::string get_humidity(); 
+		/*
+		set the ambient humidity percentage
+		*/
+		std::string set_humidity(uint8_t newValue); 
 
-private:
-	uint8_t	m_startMsgId;
-	uint8_t	m_temperatureGetMsgId;
-	uint8_t	m_temperatureSetMsgId;
-	uint8_t	m_humiditySetMsgId;
-	uint8_t	m_humidityGetMsgId;
-	 ~CAN_min_signals();
-};
-#endif /*HEADER_MIN_SIGNALS_H */
+	private:
+	 uint8_t m_startMsgID;
+	 uint8_t m_temperatureGetMsgID;
+	 uint8_t m_temperatureSetMsgID;
+	 uint8_t m_humidityGetMsgID;
+	 uint8_t m_humiditySetMsgID;};
+#endif //HEADER_MIN_SIGNALS_H
